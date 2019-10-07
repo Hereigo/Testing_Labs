292ade225c5220ca3e14bc7388853ff3044d50a1
Hereigo <Hereigo@online.ua>
2019-10-07 17:45:46 +0300

    ng rx-forms simple validation

--- Angular_iAdvanced_Rx/sample/src/app/my-form/my-form.component.ts
+++ Angular_iAdvanced_Rx/sample/src/app/my-form/my-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { emailValidator } from './email-validator';
 
 @Component({
   selector: 'app-my-form',
@@ -12,20 +13,22 @@ export class MyFormComponent implements OnInit {
 
   constructor() { }
 
+  // MODEL-DRIVEN_DEVELOPMENT  - nedded to start DEV from creating Model...
+
   ngOnInit() {
+
     this.registrationForm = new FormGroup({
+
       name: new FormGroup({
         firstName: new FormControl('', Validators.required),
         lastName: new FormControl('', Validators.required)
       }),
-      email: new FormControl('', [
-        Validators.required,
-        Validators.pattern('[^ @]*@[^ @]*')
-      ]),
-      password: new FormControl('', [
-        Validators.minLength(8),
-        Validators.required
-      ])
+
+      email: new FormControl('', [Validators.required, emailValidator]),
+
+      password: new FormControl('', [Validators.required, Validators.minLength(8)])
+
     });
   }
+
 }
