3bfd86f93bfb425629fa37032fc03ee593ec0bb1
Hereigo <Hereigo@online.ua>
2019-10-15 18:38:20 +0300

    passing and using data with event binding

--- Angular_ude/a-project/src/app/app.component.ts
+++ Angular_ude/a-project/src/app/app.component.ts
@@ -6,5 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
   title = 'a-project';
+
+  allowNewServer = false;
+
+  serverCreationStatus = 'No server was created.';
+
+  serverName = '';
+
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 3000);
+  }
+
+  onServerCreated() {
+    this.serverCreationStatus = 'Server was created!';
+  }
+
+  onUpdateServerName(event: Event) {
+
+    this.serverName = (<HTMLInputElement>event.target).value;
+
+  }
 }
