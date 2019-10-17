c2e52df2115f7e4105805f344f7985c003236218
Hereigo <Hereigo@online.ua>
2019-10-17 17:07:48 +0300

    ng u2 - bootstrap items card

--- Angular_ude_2/b-project/src/app/recipes/recipe-list/recipe-list.component.ts
+++ Angular_ude_2/b-project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
-export class RecipeListComponent implements OnInit {
+export class RecipeListComponent {
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+  recipes: Recipe[] = [
+    new Recipe('A test recipe', 'This is just a test recipe...',
+      'https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/caponata-pasta_1.jpg')
+  ];
 
 }
