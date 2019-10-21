b4a7eecd8314291646afe841ec4166bf9c40e30d
Hereigo <Hereigo@online.ua>
2019-10-21 16:39:50 +0300

    ng simpliest app

--- Angular_833_Node1016/src/main.ts
+++
@@ -1,7 +0,0 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
-import { AppModule } from './app/app.module';
-
-const platform = platformBrowserDynamic();
-
-platform.bootstrapModule(AppModule);  //  AppModule - default app start point.
--- Angular_833_Node1016/src/polyfills.ts
+++
@@ -1,3 +0,0 @@
-import 'core-js';
-// zone is polyfill that is neede for angular to work with different browsers
-import 'zone.js/dist/zone';
