61f965226e390a33a8b6ee344ef1a327ea67b264
Hereigo <Hereigo@online.ua>
2019-10-21 11:35:41 +0300

    ng ude - debug config 4 firefox + refactoring

--- JavaScript_Plural/_Hunks/closures.js
+++ JavaScript_Plural/_Hunks/closures.js
@@ -1,17 +1,30 @@
-
 console.log(" === CLOSURES : (capturing execution context of function's local var)");
 
 function getCounter() {
 
-  var counter = 0;
+    var counter = 0;
 
-  return function () {
-    return counter++;
-  }
+    return function() {
+        return counter++;
+    }
 }
 
 var getCounterStore = getCounter();
 
-console.log(getCounterStore());  // 0
-console.log(getCounterStore());  // 1
-console.log(getCounterStore());  // 2
+console.log(getCounterStore()); // 0
+console.log(getCounterStore()); // 1
+console.log(getCounterStore()); // 2
+
+console.log('=============== PART II ====================== ');
+
+function multiplier(factor) {
+
+    return function(num) {
+        return num * factor;
+    }
+}
+
+// twice = return of multiplier = function(num){...}
+let twice = multiplier(2);
+
+console.log(twice(5)); // 10
