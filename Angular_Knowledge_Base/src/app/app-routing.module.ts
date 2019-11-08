36bde02f3e9b57ff3b1ccf4dcd381afe046a4856
Hereigo <Hereigo@online.ua>
2019-11-08 19:01:42 +0200

    ng kb add routing

--- Angular_Knowledge_Base/src/app/app-routing.module.ts
+++ Angular_Knowledge_Base/src/app/app-routing.module.ts
@@ -1,8 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'pageTwo', component: HomeComponent },
+  {
+    path: 'pageThree', component: HomeComponent, children:
+      [
+        // canActivate - for authority
+        // canLoad - ...
+        // resolve (dictionary) - to check data before onInit Component
+        // loadChildren: () => import('./photos/photos.module').then(x => x.PhotosModule), 
+        //  - lazy loaded route in case of allowed (see more on angular.io)
+      ]
+  },
+  { path: '**', component: HomeComponent }
+  // { path: '**', component: NotFoundComponent } - to process "NotFoundComponent" cases.
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
