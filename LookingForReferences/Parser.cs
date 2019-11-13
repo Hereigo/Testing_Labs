41a4766ab1b353541f8e8db7887b8ae3557f0702
Hereigo <Hereigo@online.ua>
2019-11-13 19:39:20 +0200

    angular8core3 + references parser

---
+++ LookingForReferences/Parser.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LookingForReferences
+{
+    static class Parser
+    {
+        public static void ParseAndLog(string[] foldersToParse, string[] fileTypes, string filesListToSearch, string logFileName)
+        {
+            // FILES TO LOOK FOR :
+
+            List<string> filesCollectToSearch = new List<string>();
+
+            StreamReader fileToRead = null;
+            using (fileToRead = new StreamReader(filesListToSearch))
+            {
+                string line;
+
+                while ((line = fileToRead.ReadLine()) != null)
+                {
+                    filesCollectToSearch.Add(line.Trim());
+                }
+            }
+
+            // FILES WHERE TO LOOK FOR :
+
+            List<string> filesCollectToParse = new List<string>();
+
+            foreach (string folder in foldersToParse)
+            {
+                // Get List of available files with defined extensions :
+                IEnumerable<string> currentDirFiles = Directory.GetFiles(folder, "*.*", SearchOption.AllDirectories)
+                    .Where(s => fileTypes.Contains(Path.GetExtension(s).ToLower()));
+
+                filesCollectToParse.AddRange(currentDirFiles);
+            }
+
+            // PARSING :
+
+            StreamReader fileStreamToParse = null;
+
+            foreach (string fileToParse in filesCollectToParse)
+            {
+                using (fileStreamToParse = new StreamReader(fileToParse))
+                {
+                    string line;
+
+                    while ((line = fileStreamToParse.ReadLine()) != null)
+                    {
+                        // TODO :
+                        // Check IF LINE CONTAINS file name !!!
+
+                        // Write FILE NAME
+                        // may be line number...
+                        // 
+                        File.AppendAllText(logFileName, line);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ LookingForReferences/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+
+namespace LookingForReferences
+{
+    static class Program
+    {
+        private static void Main()
+        {
+            string logFileName = "REFS_LOG_" + DateTime.Now.ToString("yyMMdd_HHmmss") + ".log";
+
+            string filesListToSearch = "filesListToSearch.txt";
+
+            string[] foldersToParse = {
+                @"C:\Users\user\Downloads",
+                @"C:\Users\user\Documents"
+            };
+
+            string[] fileTypesToParse = { ".asp", ".asp_bk", ".aspx", ".js", ".vb", ".htm", ".html", ".ini", ".config", ".ascx", ".asax", ".xml" };
+
+            try
+            {
+                Parser.ParseAndLog(foldersToParse, fileTypesToParse, filesListToSearch, logFileName);
+            }
+            catch (UnauthorizedAccessException authEx)
+            {
+                Console.WriteLine("NO ACCESS TO :");
+                Console.WriteLine(authEx.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("ERROR : ");
+                Console.WriteLine(ex);
+            }
+            Console.WriteLine("FINISHED.");
+            Console.ReadKey();
+        }
+    }
+}
