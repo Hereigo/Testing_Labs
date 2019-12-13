68910848bbe0e372aa9f73f5da14f40431e341da
Hereigo <Hereigo@online.ua>
2019-12-13 17:40:17 +0200

    work with ado extensions + some js

--- JAVA_SCRIPT/let_inside_func.js
+++ JAVA_SCRIPT/let_inside_func.js
@@ -35,3 +35,19 @@ function newUser(dataUser) {
 newUser(dataUser);
 
 console.log(dataUser.name); // Robin
+
+// =============================================
+
+for (var i = 0; i < 5; i++) {
+    setTimeout(function () {
+        console.log(i);
+    });
+}
+// output for VAR index - 5,5,5,5,5
+
+for (let i = 0; i < 5; i++) {
+    setTimeout(function () {
+        console.log(i);
+    });
+}
+// output for VAR index - 0,1,2,3,4
--- JAVA_SCRIPT/z_TESTS.js
+++ JAVA_SCRIPT/z_TESTS.js
@@ -1,13 +1,48 @@
-for (var i = 0; i < 5; i++) {
-    setTimeout(function () {
-        console.log(i);
-    });
+function fn(value) {
+    return true - value;
 }
-// output for VAR index - 5,5,5,5,5
 
-for (let i = 0; i < 5; i++) {
-    setTimeout(function () {
-        console.log(i);
-    });
+console.log(fn('4'));   // -3
+console.log(fn('-4'));  // 5
+console.log(fn(-'4'));  // 5
+console.log(fn(4));     // -3
+
+// ==================================
+
+function isTruthVal(value) {
+    if (value)
+        console.log("is Truth.");
+    else console.log("is Not Truth.");
 }
-// output for VAR index - 0,1,2,3,4
+
+isTruthVal(undefined);// is Not Truth.
+isTruthVal(NaN);     // is Not Truth.
+isTruthVal(null);    // is Not Truth.
+isTruthVal(0);       // is Not Truth.
+isTruthVal("");      // is Not Truth.
+isTruthVal(false);   // is Not Truth.
+isTruthVal("false"); // is Truth.
+isTruthVal(1);       // is Truth.
+isTruthVal(-1);      // is Truth.
+isTruthVal([]);      // is Truth.
+isTruthVal({});      // is Truth.
+
+// ==================================
+
+[1, 2, 3, 4, 5].map(console.log);
+
+// The above is equivalent to :
+[1, 2, 3, 4, 5].map(
+    (val, index, array) => console.log(val, index, array)
+);
+
+// 1 0 [ 1, 2, 3, 4, 5 ]
+// 2 1 [ 1, 2, 3, 4, 5 ]
+// 3 2 [ 1, 2, 3, 4, 5 ]
+// 4 3 [ 1, 2, 3, 4, 5 ]
+// 5 4 [ 1, 2, 3, 4, 5 ]
+// 1 0 [ 1, 2, 3, 4, 5 ]
+// 2 1 [ 1, 2, 3, 4, 5 ]
+// 3 2 [ 1, 2, 3, 4, 5 ]
+// 4 3 [ 1, 2, 3, 4, 5 ]
+// 5 4 [ 1, 2, 3, 4, 5 ]
