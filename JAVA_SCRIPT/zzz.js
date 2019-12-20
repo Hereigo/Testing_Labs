22c0eda1ccda384756bb678a3d6a8915458b122a
Hereigo <Hereigo@online.ua>
2019-12-20 16:11:55 +0200

    small updates

--- JAVA_SCRIPT/zzz.js
+++ JAVA_SCRIPT/zzz.js
@@ -7,3 +7,11 @@ console.log(name); // John
 let [x, y, z] = [1, 2, 3, 4, 5];
 
 console.log(z); // 3
+
+// JSON :
+
+let string = JSON.stringify({ squirrel: false, events: ["weekend"] });
+
+console.log(string);  // {"squirrel":false,"events":["weekend"]};
+
+console.log(JSON.parse(string).events);  // ['weekend']
