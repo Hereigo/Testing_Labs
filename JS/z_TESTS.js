70e9cfac0d28a6d60f2609e46a1db7824a3ecb48
Hereigo <Hereigo@online.ua>
2019-12-23 15:40:41 +0200

    zd successful add comment

--- JS/z_TESTS.js
+++ JS/z_TESTS.js
@@ -14,20 +14,22 @@ function isTruthVal(value) {
         console.log("is Truth.");
     else console.log("is Not Truth.");
 }
-
+// is FALSE :
+isTruthVal(NaN == NaN);       // is Not Truth.
 isTruthVal(undefined);// is Not Truth.
 isTruthVal(NaN);     // is Not Truth.
 isTruthVal(null);    // is Not Truth.
 isTruthVal(0);       // is Not Truth.
 isTruthVal("");      // is Not Truth.
 isTruthVal(false);   // is Not Truth.
+// is TRUE :
 isTruthVal("false"); // is Truth.
 isTruthVal(1);       // is Truth.
 isTruthVal(-1);      // is Truth.
 isTruthVal([]);      // is Truth.
 isTruthVal({});      // is Truth.
 isTruthVal(null == undefined);// is Truth.
-isTruthVal(NaN == NaN);       // is Not Truth.
+isTruthVal(typeof (null) == "object"); // is Truth.
 
 // ==================================
 
