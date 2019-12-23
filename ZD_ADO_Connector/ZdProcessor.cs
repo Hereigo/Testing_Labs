7d3f2c9439792aedaa6ade836329ee538e762137
Hereigo <Hereigo@online.ua>
2019-12-23 11:46:31 +0200

    zd add comments

--- ZD_ADO_Connector/ZdProcessor.cs
+++ ZD_ADO_Connector/ZdProcessor.cs
@@ -7,53 +7,62 @@ namespace ZD_ADO_Connector
 {
     internal class ZdProcessor
     {
-        //private HttpMethod httpMethod = HttpMethod.Get;
-
-        public void ZendeskApiClient()
+        public void ApiClientForZendesk()
         {
-            const long adoCustomFieldId = 360004572998;
+            // 1. GET ZD-TICKETS 
 
-            const string adoTicketsTag = "azure";
+            GetSomeTickets();
 
-            // 1. GET ZD-TICKETS CREATED NOT MORE THAN ... ( X HOURS AGO )
+            SendComment();
 
-            // string requestByTime = $"{GIT_IGNORE.Variables.ZdApiRootPath}?page=0&query=type:ticket status:open created>{notEarlierThan.ToString("yyyy-MM-ddTHH:mm:ssZ")}";
-            // Add 5 hours for the difference of Time Zones :
-            DateTime notEarlierThan = DateTime.Now.AddHours(-30);
-
-            // "custom_fields": [{"id": 360004572998, ... (my own created ID)
-            // search possible only by TAG !!!
-            string requestByTime = $"{GIT_IGNORE.Variables.ZdApiRootPath}?page=0&query=type:ticket tags:{adoTicketsTag}";
-
-            Task<string> response = RequestSend(requestByTime);
-
-            string rez = response.Result;
-
-
-            Console.WriteLine(rez);
             // 2. CHECK  (BY ...) IF ADO NOT CONTAINS THEM YET
 
             // 3. IF NOT - CREATE NEW IN ADO
 
         }
 
-        private async Task<string> RequestSend(string _apiRequest) //, HttpMethod httpMethod, string jsonToSend)
+        private void GetSomeTickets()
+        {
+            const long adoCustomFieldId = 360004572998;
+            const string adoTicketsTag = "azure";
+
+            // CREATED NOT MORE THAN ... ( X HOURS AGO )
+            // string requestByTime = $"{GIT_IGNORE.Variables.ZdApiRootPath}?page=0&query=type:ticket status:open created>{notEarlierThan.ToString("yyyy-MM-ddTHH:mm:ssZ")}";
+            // Add 5 hours for the difference of Time Zones :
+            DateTime notEarlierThan = DateTime.Now.AddHours(-30);
+
+            // "custom_fields": [{"id": 360004572998, ... (my own created ID)
+            // search possible only by TAG !!!
+            string requestByTime = $"{GIT_IGNORE.Variables.ZdApiRootPath}/api/v2/search.json?page=0&query=type:ticket tags:{adoTicketsTag}";
+
+            Task<string> response = RequestSend(HttpMethod.Get, requestByTime);
+
+            string rez = response.Result;
+            Console.WriteLine(rez);
+        }
+
+        private async Task<string> RequestSend(HttpMethod httpMethod, string apiRequest, string jsonToSend = "") //, HttpMethod httpMethod, string jsonToSend)
         {
             //string requestUri = MagicWords.ZendeskRequestBaseUrl + _apiRequest; // "/api/v2/search.json?query=82385";
 
-            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, _apiRequest);
+            HttpRequestMessage request = new HttpRequestMessage(httpMethod, apiRequest);
 
             request.Headers.Add("Accept", "application/json");// Generated with https://developer.zendesk.com/requests/new
 
             request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", GIT_IGNORE.Variables.ZDDevConsoleBearerToken);
 
-            //request.Content = new StringContent(jsonToSend, System.Text.Encoding.UTF8, "application/json");
+            if (httpMethod == HttpMethod.Put)
+            {
+                string jsonMultiline = jsonToSend.Replace("\r\n", "\\n");
+
+                request.Content = new StringContent(jsonMultiline, System.Text.Encoding.UTF8, "application/json");
+            }
 
             using HttpClient client = new HttpClient();
 
             HttpResponseMessage response = await client.SendAsync(request).ConfigureAwait(false);
 
-            string logInformation = $"Tasker.SendRequestToZendesk() status = {response.StatusCode} - path: {_apiRequest}";
+            string logInformation = $"Tasker.SendRequestToZendesk() status = {response.StatusCode} - path: {apiRequest}";
 
             if (response.IsSuccessStatusCode)
             {
@@ -65,12 +74,24 @@ namespace ZD_ADO_Connector
             {
                 //_logger.LogError(logInformation);
 
-                return response.StatusCode.ToString();
+                return "Error - " + response.StatusCode.ToString();
             }
         }
 
+        private void SendComment(long ticketId = 0, string jsonToSend = "", long supportUserId = 0)
+        {
+            ticketId = 3;
 
+            supportUserId = 368907442298;
 
-        //
+            jsonToSend = "{\"ticket\": { \"comment\": { \"body\": \"My new Internal note =)\", \"public\":\"false\", \"author_id\": " + supportUserId + " }}}";
+
+            string apiRequest = $"{GIT_IGNORE.Variables.ZdApiRootPath}/api/v2/tickets/{ticketId}.json";
+
+            Task<string> response = RequestSend(HttpMethod.Put, apiRequest, jsonToSend);
+
+            string rez = response.Result;
+            Console.WriteLine(rez);
+        }
     }
 }
