22b56a89e4bc8ea7a93832f8b56bdef244da8016
Hereigo <Hereigo@online.ua>
2020-01-06 16:23:46 +0200

    empty angular-js + dotnet 4.6.2 app

---
+++ Angular_Net462/App_Start/BundleConfig.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Optimization;
+
+namespace Angular_Net462
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/site.css"));
+
+            BundleTable.EnableOptimizations = true;
+        }
+    }
+}
---
+++ Angular_Net462/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Angular_Net462.Startup))]
+namespace Angular_Net462
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
