fe4c86b801c1991febbaa37988c6236c9ff94b71
Hereigo <Hereigo@online.ua>
2020-01-08 12:52:39 +0200

    update packages

---
+++ SQL_Stored_Procedures/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SQL_Stored_Procedures
+{
+    class Program
+    {
+        string connStr = "Data Source=(localdb)\\MSSQLLocalDB;Integrated Security=True;";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+// connectionString="Data Source=(localdb)\MSSQLLocalDB; Initial Catalog=PaymentsContext-2019-05-07; Integrated Security=True; MultipleActiveResultSets=True; AttachDbFilename=|DataDirectory|PaymentsContext-2019-05-07.mdf"
+// providerName="System.Data.SqlClient"
